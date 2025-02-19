009a42c607d843c45c41e8c0923d127d71e90ba2
Benjamin Kamanda <benjamin.kamanda@stu.ocsb.ca>
2025-02-19 17:57:02 +0000

    Made basic math.py

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created by: Ben
+# Date: February 19 2025
+# Does basic math
+
+
+def main():
+    # calculates area
+    print("5-2={}".format(5 - 2))
+    print("7+4={}".format(7 + 4))
+    print("4*4={}".format(4 * 4))
+    print("8/2={}".format(8 / 2))
+    print("2+3*4={}".format(2 + 3 * 4))
+    print("3^2={}".format(3 ^ 2))
+    print("5**2={}".format(5**2))
+    print("13**0.5={}".format(13**0.5))
+
+
+if __name__ == "__main__":
+    main()
